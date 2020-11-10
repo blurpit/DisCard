b24549b8d593508e237be2d84402de99258bb102
david <davidschneider821@gmail.com>
2020-11-10 16:35:52 -0700

    Fix image dumper

--- img_dumper.py
+++ img_dumper.py
@@ -1,6 +1,8 @@
 import asyncio
+import datetime as dt
 
 import discord as d
+
 import db
 
 client = d.Client()
@@ -14,29 +16,36 @@ async def on_ready():
     print("\nLogged in as {}".format(client.user))
 
     channel:d.TextChannel = client.get_channel(DUMP_CHANNEL)
-    # definitions = db.session.query(db.CardDefinition) \
-    #     .limit(DUMP_LIMIT) \
-    #     .offset(DUMP_START - 1) \
-    #     .all()
-    #
-    # for definition in definitions:
-    #     file = d.File(f'data/images/{definition.id}.png')
-    #     msg = await channel.send(file=file)
-    #     definition.image_id = msg.id
-    #     db.session.commit()
-    #
-    #     print(definition.id, definition.image_id, file)
-    #     await asyncio.sleep(DUMP_DELAY)
+    definitions = db.session.query(db.CardDefinition) \
+        .order_by(db.CardDefinition.id) \
+        .limit(DUMP_LIMIT) \
+        .offset(DUMP_START - 1) \
+        .all()
 
-    async for msg in channel.history(limit=None, oldest_first=True):
-        if msg.attachments:
-            file = msg.attachments[0]
-            filename = file.filename[:-4]
-            image_id = int(file.url.rsplit('/', 2)[1])
-            card_id = int(filename) if filename.isnumeric() else filename
-            print(card_id, image_id, sep='\t')
+    for definition in definitions:
+        file = d.File(f'data/images/{definition.id}.png')
+        msg = await channel.send(file=file)
 
-    # db.session.commit()
+        card_id, image_id = get_img_info(msg)
+        assert card_id == definition.id, 'ID MISMATCH ON ' + str(definition.id) + ', ' + str(card_id)
+        definition.image_id = image_id
+        db.session.commit()
+
+        print(definition.id, definition.image_id, file)
+        await asyncio.sleep(DUMP_DELAY)
+
+    # async for msg in channel.history(limit=None, oldest_first=True, after=None):
+    #     if msg.attachments:
+    #         card_id, image_id = get_img_info(msg)
+    #         print(card_id, image_id, sep='\t')
+
+
+def get_img_info(message):
+    file = message.attachments[0]
+    filename = file.filename[:-4] # remove '.png'
+    image_id = int(file.url.rsplit('/', 2)[1])
+    card_id = int(filename) if filename.isnumeric() else filename
+    return card_id, image_id
 
 
 if __name__ == '__main__':
